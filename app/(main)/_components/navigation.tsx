ad59ff43ce41c92acd6dfa67dbf4734600790d33
Tsukishima1 <1732124764@qq.com>
2023-12-25 20:59:25 +0800

    feat: 完成侧边栏文档列表的构建

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -1,19 +1,23 @@
 "use client";
 
 import { cn } from "@/lib/utils";
-import {  ChevronsLeft, MenuIcon } from "lucide-react";
+import {  ChevronsLeft, MenuIcon, PlusCircle, Search, Settings } from "lucide-react";
 import { usePathname } from "next/navigation";
 import { ElementRef, useEffect, useRef, useState } from "react";
 import { useMediaQuery } from "usehooks-ts";
 import { UserItem } from "./user-item";
-import { useQuery } from "convex/react";
+import { useMutation } from "convex/react";
 import { api } from "@/convex/_generated/api";
+import { Item } from "./item";
+import { create } from '../../../convex/documents';
+import { toast } from "sonner";
+import { DocumentList } from "./documentlist";
 
 export const Navigation = () => {
     const pathname = usePathname();
     const isMobile = useMediaQuery("(max-width: 768px)"); // 用于判断是否是移动端
-    const documents = useQuery(api.documents.get); // 获取所有的文档
-
+    const create = useMutation(api.documents.create)
+    
     const isResizingRef = useRef(false);
     const sidebarRef = useRef<ElementRef<"aside">>(null);
     const navbarRef = useRef<ElementRef<"div">>(null);
@@ -97,6 +101,17 @@ export const Navigation = () => {
             , 300);
         }
     }
+    // 创建文章
+    const handleCreate = () => {
+        const promise = create({ title: "Untitled" });
+        // 通过 toast.promise 来显示 loading 和 success 或者 error
+
+        toast.promise(promise, {
+            loading: "Creating a new note...",
+            success: "New note created!!",
+            error: "Failed to create a new note."
+        });
+    }
 
     return (
     <>
@@ -119,13 +134,25 @@ export const Navigation = () => {
             </div>
             <div>
                 <UserItem/>
+                <Item 
+                    label="Search"
+                    icon={Search}
+                    isSearch
+                    onClick={() => {}}
+                />
+                <Item 
+                    label="Settings"
+                    icon={Settings}
+                    onClick={() => {}}
+                />
+                <Item 
+                    onClick={handleCreate} 
+                    label="New Page" 
+                    icon={PlusCircle}
+                />
             </div>
             <div className="mt-4">
-                {documents?.map((document) => (
-                    <p key={document._id}>
-                        {document.title}
-                    </p>
-                ))}
+                <DocumentList />
             </div>
             <div
                 onMouseDown={handleMouseDown}
--- convex/documents.ts
+++ convex/documents.ts
@@ -2,6 +2,31 @@ import { v } from "convex/values";
 import { mutation, query } from "./_generated/server";
 import { Doc,Id } from "./_generated/dataModel";
 
+export const getSidebar = query({
+    args: {
+        parentDocument: v.optional(v.id("documents")),
+    },
+    handler: async (ctx, args) => {
+        const identity = await ctx.auth.getUserIdentity(); 
+        if (!identity) {
+            throw new Error("Unauthorized"); 
+        }
+        const userId = identity.subject; 
+        const documents = await ctx.db 
+        .query("documents") // 查询数据库中的documents表
+        .withIndex("by_user_parent",(q)=> 
+          q
+            .eq("userId", userId)
+            .eq("parentDocument", args.parentDocument)
+        ) // 按照用户id和父文档查询
+        .filter((q)=>q.eq(q.field("isArchived"),false)) // 过滤掉已归档的文档
+        .order("desc") // 按照降序排列
+        .collect(); // 转换为数组
+
+        return documents;
+    }
+})
+
 export const get = query({
     handler: async (ctx) => {
         const identity = await ctx.auth.getUserIdentity(); // 这里指的是获取用户身份
