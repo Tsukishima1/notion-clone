dea146b43d7f01404209ae20e2d12fa0e7ab74ac
Tsukishima1 <1732124764@qq.com>
2024-01-16 12:23:16 +0800

    feat: 增加警告对话框组件，用于永久删除笔记的确认

--- app/(main)/_components/trash-box.tsx
+++ app/(main)/_components/trash-box.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { ConfirmModal } from "@/components/modals/confirm-modal";
 import { Spinner } from "@/components/spinner";
 import { Input } from "@/components/ui/input";
 import { api } from "@/convex/_generated/api";
@@ -97,9 +98,11 @@ export const TrashBox = () => {
                             >
                                 <Undo className="w-4 h-4 text-muted-foreground"/>
                             </div>
+                            <ConfirmModal onConfirm={()=>onRemove(document._id)}>
                             <div role="button" className="rounded-sm p-2 hover:bg-neutral-200">
-                            <Trash className="h-4 w-4 text-muted-foreground" onClick={(e)=>{e.stopPropagation()}}/>
+                            <Trash className="h-4 w-4 text-muted-foreground"/>
                             </div>
+                            </ConfirmModal>
                         </div>
                     </div>
                 ))}
