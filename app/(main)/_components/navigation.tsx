1aa7926dc90b080e38650a8c3e823379600e888e
Tsukishima1 <1732124764@qq.com>
2024-01-28 19:55:35 +0800

    fix: 移动端导航栏打开时溢出没被隐藏的问题

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -192,7 +192,7 @@ export const Navigation = () => {
         <div
             ref={navbarRef}
             className={cn(
-                "absolute top-0 z-[100] left-60 w-[calc(100%-240px)]",
+                "absolute top-0 z-[100] left-60 w-[calc(100%-240px)] overflow-hidden",
                 isResetting && "transition-all ease-in-out duration-300",
                 isMobile && "left-0 w-full",
             )}
